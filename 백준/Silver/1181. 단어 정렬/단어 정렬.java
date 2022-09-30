d0502d7cccf04eafd114f8b2215bc04231e4a197
percyfrank <85394884+percyfrank@users.noreply.github.com>
2022-09-30 17:42:57 +0900

    [Silver V] Title: 단어 정렬, Time: 528 ms, Memory: 22084 KB -BaekjoonHub

---
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.java
@@ -0,0 +1,42 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Comparator;
+
+/**
+ * 단어 정렬 - 1181
+ */
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuffer sb = new StringBuffer();
+        int n = Integer.parseInt(br.readLine());
+        String[] arr = new String[n];
+
+        for(int i = 0; i < n; i++) {
+            arr[i] = br.readLine();
+        }
+
+        Arrays.sort(arr, new Comparator<String>() {
+            @Override
+            public int compare(String o1, String o2) {
+                if(o1.length() == o2.length()) {
+                    return o1.compareTo(o2);
+                } else {
+                    return o1.length()-o2.length();
+                }
+            }
+        });
+        
+        System.out.println(arr[0]);
+        for(int i = 1; i < n; i++) {
+            if(arr[i].equals(arr[i-1])) {
+            } else{
+                System.out.println(arr[i]);
+            }
+        }
+
+    }
+}
