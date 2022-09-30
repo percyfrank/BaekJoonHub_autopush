e166a6790849b4d5bd408cecfe626e456abb89a3
percyfrank <85394884+percyfrank@users.noreply.github.com>
2022-09-30 11:59:33 +0900

    [Bronze I] Title: 이 구역의 승자는 누구야?!, Time: 260 ms, Memory: 21992 KB -BaekjoonHub

--- 백준/Bronze/20154. 이 구역의 승자는 누구야？！/이 구역의 승자는 누구야？！.java
+++ 백준/Bronze/20154. 이 구역의 승자는 누구야？！/이 구역의 승자는 누구야？！.java
@@ -11,22 +11,15 @@ public class Main {
         char[] arr = {3,2,1,2,3,3,3,3,1,1,3,1,3,3,1,2,2,2,1,2,1,1,2,2,2,1};
 
         String s = br.readLine();
+        char[] chr = s.toCharArray();
+        int sum = 0;
 
-        int[] arrcnt = new int[s.length()];
-        for(int i = 0 ; i < s.length();i++) {
-            for(int j = 0; j < arr.length; j++) {
-                if(s.charAt(i) == ((char)(j+'A'))) {
-                    arrcnt[i] = arr[j];
-                }
-            }
+        for(int i = 0; i < chr.length; i++) {
+            sum += arr[chr[i]-'A'];
         }
 
-        int sum = 0;
-        for(int i = 0; i < arrcnt.length; i ++){
-            sum += arrcnt[i];
-            if(sum > 10) {
-                sum %= 10;
-            }
+        if(sum > 10) {
+            sum %= 10;
         }
 
         System.out.println(sum % 2 == 1 ? "I'm a winner!" : "You're the winner?");
