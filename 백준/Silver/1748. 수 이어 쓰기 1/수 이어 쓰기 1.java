9e4bdbcc0b04e5565f261b93e3417bdd56bccef9
percyfrank <85394884+percyfrank@users.noreply.github.com>
2022-10-05 03:23:16 +0900

    [Silver IV] Title: 수 이어 쓰기 1, Time: 124 ms, Memory: 14132 KB -BaekjoonHub

--- 백준/Silver/1748. 수 이어 쓰기 1/수 이어 쓰기 1.java
+++ 백준/Silver/1748. 수 이어 쓰기 1/수 이어 쓰기 1.java
@@ -1,27 +1,32 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 
+/**
+ * 수 이어 쓰기 1 - 1748번
+ */
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
 
-        Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
         int n = N;
-        int cnt = 0;
-        int sum = 0;
+        int sum = 0; // 
+        int cnt = 0; // N의 자릿수
 
-        // 입력된 N의 자릿수 구하기
-        while(true) {
-            n  = (int) n / 10;
+        while (true) {
+            n /= 10;
             cnt++;
-            if (n == 0) break;
+            if(n == 0) break;
         }
 
-        for (int i = 1; i < cnt; i++) {
-            sum += (int) (i * (9 * Math.pow(10,i-1)));
+        for(int i = 1; i < cnt; i++) {
+            sum += i * (9 * (Math.pow(10,i-1)));
         }
 
-        sum += (int) (cnt * (N-Math.pow(10,cnt-1)+1));
+        sum += (N-Math.pow(10,cnt-1)+1) * cnt;
 
         System.out.println(sum);
+
     }
 }
