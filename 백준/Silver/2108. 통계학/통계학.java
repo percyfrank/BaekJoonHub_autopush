1f80b51c5b38bc3111f25b9a5a42814e158ba3ba
percyfrank <85394884+percyfrank@users.noreply.github.com>
2022-10-05 02:58:01 +0900

    [Silver III] Title: 통계학, Time: 760 ms, Memory: 69456 KB -BaekjoonHub

--- 백준/Silver/2108. 통계학/통계학.java
+++ 백준/Silver/2108. 통계학/통계학.java
@@ -1,19 +1,21 @@
-import java.util.Arrays;
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.*;
 
 /**
  * 통계학 - 2108번
  */
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
 
-        Scanner sc = new Scanner(System.in);
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//        Scanner sc = new Scanner(System.in);
 
-        int N = sc.nextInt();
+        int N = Integer.parseInt(br.readLine());
         int[] arr = new int[N];
 
         for (int i = 0; i < N; i++) {
-            arr[i] = sc.nextInt();
+            arr[i] = Integer.parseInt(br.readLine());
         }
 
         double sum = 0;
@@ -33,28 +35,31 @@ public class Main {
         System.out.println(arr[mid]);
 
         //최빈값
-        boolean flag = true; //최빈값이 첫번째이면 false 두번째이면 true
-        int mod = arr[0]; //최빈값(정수가 한개일 땐 그 즉시 최빈값)
-        int max = Integer.MIN_VALUE; //최빈값 빈도의 최댓값
-        int cnt = 0; //최빈값이 몇번 등장하는지
+        HashMap<Integer, Integer> map = new HashMap<>();
+        //최빈값이 여러개일 경우의 최빈값 저장 리스트
+        ArrayList<Integer> list = new ArrayList<>();
 
-        for(int i = 0; i < N-1; i++) {
-            if(arr[i] == arr[i+1]) {
-                cnt++;
-            } else {
-                cnt = 0;
-            }
+        // 키인 i가 이미 존재한다면 그 값을 가져오고, 아니면 defaultValue 대입
+        for(int i : arr) {
+            map.put(i,map.getOrDefault(i,0)+1);
+        }
 
-            if(max < cnt) {
-                max = cnt;
-                mod = arr[i];
-                flag = false;
-            } else if(max == cnt && flag == false) {
-                mod = arr[i];
-                flag = true;
+        int max = Integer.MIN_VALUE;
+        for(int value : map.values()) {
+            max = Math.max(max,value);
+        }
+
+        Iterator it = map.entrySet().iterator();
+        while (it.hasNext()) {
+            Map.Entry entry = (Map.Entry)it.next();
+            if((int)entry.getValue() == max) {
+                list.add((int)entry.getKey());
             }
         }
-        System.out.println(mod);
+
+        Collections.sort(list);
+        if(list.size() == 1) System.out.println(list.get(0));
+        else System.out.println(list.get(1));
 
         //범위
         System.out.println(arr[N - 1] - arr[0]);
