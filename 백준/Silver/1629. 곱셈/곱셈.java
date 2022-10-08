d24d059df2c6cb6f18458a2760295706bb4eb797
percyfrank <85394884+percyfrank@users.noreply.github.com>
2022-10-09 05:14:12 +0900

    [Silver I] Title: 곱셈, Time: 124 ms, Memory: 14184 KB -BaekjoonHub

---
+++ 백준/Silver/1629. 곱셈/곱셈.java
@@ -0,0 +1,36 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+/**
+ * 곱셈 - 1629번
+ */
+public class Main {
+    public static void main(String[] args) throws Exception {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        long A = Long.parseLong(st.nextToken());
+        long B = Long.parseLong(st.nextToken());
+        long C = Long.parseLong(st.nextToken());
+
+        System.out.println(modBy(A,B,C));
+    }
+
+    public static long modBy(long base,long idx,long mod) {
+       
+        if(idx == 1) {
+            return base % mod;
+        }
+
+        long r = modBy(base, idx / 2, mod);
+        if (idx % 2 == 1) {
+            return (r * r % mod) * base % mod;
+        } else {
+            return (r * r) % mod;
+        }
+
+    }
+}
