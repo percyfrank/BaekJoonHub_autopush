74ccb63d8afbd47755d0f0241eb72bcd591c42f6
percyfrank <85394884+percyfrank@users.noreply.github.com>
2022-10-11 09:28:31 +0900

    [level 1] Title: 자릿수 더하기, Time: 0.03 ms, Memory: 79.8 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12931. 자릿수 더하기/자릿수 더하기.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Solution {
+    public int solution(int n) {
+        String s = String.valueOf(n);
+        int sum = 0;
+        for(int i = 0; i < s.length(); i++) {
+            sum += Integer.parseInt(String.valueOf(s.charAt(i)));
+        }
+
+        return sum;
+    }
+}
