22c75015c86852830e1e536e8aa4efdcf2fc8c6e
percyfrank <85394884+percyfrank@users.noreply.github.com>
2022-10-12 00:02:08 +0900

    [Silver V] Title: 배열 합치기, Time: 1524 ms, Memory: 308376 KB -BaekjoonHub

--- 백준/Silver/11728. 배열 합치기/배열 합치기.java
+++ 백준/Silver/11728. 배열 합치기/배열 합치기.java
@@ -16,58 +16,25 @@ public class Main {
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
 
-        int[] arrA = new int[N];
-        int[] arrB = new int[M];
-        int[] arrC = new int[N+M];
-        StringTokenizer st1 = new StringTokenizer(br.readLine());
-        for(int i = 0; i < N; i++) {
-            arrA[i] = Integer.parseInt(st1.nextToken());
+        
+        int[] arr = new int[N + M];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
         }
 
-        StringTokenizer st2 = new StringTokenizer(br.readLine());
-        for(int j = 0; j < M; j++) {
-            arrB[j] = Integer.parseInt(st2.nextToken());
+        st = new StringTokenizer(br.readLine());
+        for (int j = N; j < N + M; j++) {
+            arr[j] = Integer.parseInt(st.nextToken());
 
         }
 
-        Arrays.sort(arrA);
-        Arrays.sort(arrB);
-
-        int a = 0;
-        int b = 0;
-        int c = 0;
-        while(true) {
-            if(arrA[a] >= arrB[b]) {
-                arrC[c] = arrB[b];
-                c++;
-                b++;
-                if(b == M) {
-                    while(a != N) {
-                        arrC[c] = arrA[a];
-                        c++;
-                        a++;
-                    }
-                    break;
-                }
-            } else{
-                arrC[c] = arrA[a];
-                c++;
-                a++;
-                if(a == N) {
-                    while(b != M) {
-                        arrC[c] = arrB[b];
-                        c++;
-                        b++;
-                    }
-                    break;
-                }
-            }
-        }
+        Arrays.sort(arr);
         StringBuffer sb = new StringBuffer();
-        for(int i : arrC) {
+        for(int i : arr) {
             sb.append(i).append(" ");
         }
         System.out.println(sb);
-        
     }
 }
