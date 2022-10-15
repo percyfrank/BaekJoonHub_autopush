bd4960419b852ade6ab2db288712955613d95609
percyfrank <85394884+percyfrank@users.noreply.github.com>
2022-10-16 02:41:03 +0900

    [Silver I] Title: 귀여운 라이언, Time: 420 ms, Memory: 73652 KB -BaekjoonHub

--- 백준/Silver/15565. 귀여운 라이언/귀여운 라이언.java
+++ 백준/Silver/15565. 귀여운 라이언/귀여운 라이언.java
@@ -1,65 +1,53 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.StringTokenizer;
 
-/**
- * 귀여운 라이언 - 15565번
- */
 public class Main {
-    public static void two_pointer(int N, int K, int[] arr) {
-
-        int left = 0;
-        int right = 0;
-        int cnt = 0;
-        int size = Integer.MAX_VALUE;
-
-        while(right < N) {
-            if(cnt < K){
-                if(arr[right] == 1){
-                    cnt++;
-                }
-                right++;
-            }
-            else {
-                if(arr[left] == 1) {
-                    size = Math.min(size,right-left);
-                    cnt--;
-                }
-                left++;
-            }
-        }
-
-        while(left < N) {
-            if(cnt < K) break;
-            if(arr[left] == 1) {
-                size = Math.min(size, right - left);
-                cnt--;
-            }
-            left++;
-
-        }
-
-        if(size == Integer.MAX_VALUE)
-            System.out.println(-1);
-        else
-            System.out.println(size);
-
-    }
+    
+    static int N;
+    static int K;
+    static int[] arr;
+    
     public static void main(String[] args) throws Exception {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        int N = Integer.parseInt(st.nextToken());
-        int K = Integer.parseInt(st.nextToken());
+        N = Integer.parseInt(st.nextToken());
+        K = Integer.parseInt(st.nextToken());
 
-        int[] arr = new int[N];
+        arr = new int[N];
         st = new StringTokenizer(br.readLine());
-        for(int i = 0; i < N; i++) {
+        for (int i = 0; i < N; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
         }
 
-        two_pointer(N,K,arr);
+        two_pointer();
+    }
+
+    public static void two_pointer() {
+
+        int left = 0;
+        int right = 0;
+
+        int cnt = 0;
+        int size = Integer.MAX_VALUE;
+
+        while(true) {
+            if (cnt < K) {
+                if (right == N)
+                    break;
+                if (arr[right] == 1)
+                    cnt++;
+                right++;
+            } else {
+                size = Math.min(size, right - left);
+                if (arr[left] == 1)
+                    cnt--;
+                left++;
+            }
+        }
+        System.out.println(size == Integer.MAX_VALUE ? -1 : size);
     }
 }
