1fc2d5340ec30c178b66fad4b3bea55b0bbec5ff
ohsuk Kwon <85394884+percyfrank@users.noreply.github.com>
2022-12-12 23:17:37 +0900

    [level 2] Title: 소수 찾기, Time: 15.16 ms, Memory: 83.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42839. 소수 찾기/소수 찾기.java
@@ -0,0 +1,47 @@
+import java.util.HashSet;
+
+class Solution {
+    public static char[] numArray;
+    public static boolean[] visited;
+    public static HashSet<Integer> set = new HashSet<>();;
+    
+    public static boolean isPrime(int n) {
+        if(n < 2) return false;
+        if(n == 2) return true;
+
+        for (int i = 2; i < Math.sqrt(n) + 1; i++) {
+            if(n % i == 0) return false;
+        }
+        return true;
+    }
+
+    public static void dfs(String num, int idx) {
+
+        if(num != "") set.add(Integer.parseInt(num));
+
+        if(idx == numArray.length) return;
+
+        for (int i = 0; i < numArray.length; i++) {
+            if(visited[i]) continue;
+            visited[i] = true;
+            dfs(num + numArray[i], idx + 1);
+            visited[i] = false;
+        }
+
+    }
+
+    public static int solution(String numbers) {
+        int answer = 0;
+        
+        numArray = numbers.toCharArray();
+        visited = new boolean[numbers.length()];
+
+        dfs("", 0);
+
+        for (Integer integer : set) {
+            if(isPrime(integer)) answer++;
+        }
+
+        return answer;
+    }
+}
