eef71b011e2032c90ac290ae1cb1f3695a6cbcb1
ohsuk Kwon <85394884+percyfrank@users.noreply.github.com>
2023-02-24 14:33:21 +0900

    [level 1] Title: K번째수, Time: 0.46 ms, Memory: 78.4 MB -BaekjoonHub

--- 프로그래머스/lv1/42748. K번째수/K번째수.java
+++ 프로그래머스/lv1/42748. K번째수/K번째수.java
@@ -1,15 +1,11 @@
 import java.util.*;
 class Solution {
     public int[] solution(int[] array, int[][] commands) {
+        
         int[] answer = new int[commands.length];
         
-        for(int i=0; i<commands.length; i++) {
-            int size = commands[i][1] - commands[i][0] + 1;
-            int[] tmp = new int[size];
-            int idx = 0; 
-            for(int j=commands[i][0]-1; j<commands[i][1]; j++) {
-                tmp[idx++] = array[j];
-            }
+        for(int i=0; i<commands.length; i++) {    
+            int[] tmp = Arrays.copyOfRange(array, commands[i][0]-1, commands[i][1]);
             Arrays.sort(tmp);
             answer[i] = tmp[commands[i][2]-1];
         }
