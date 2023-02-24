4b47669709abfbc80dc8a25b9e09a133645d2c53
ohsuk Kwon <85394884+percyfrank@users.noreply.github.com>
2023-02-24 10:42:03 +0900

    [level 2] Title: 주식가격, Time: 12.98 ms, Memory: 66.3 MB -BaekjoonHub

--- 프로그래머스/lv2/42584. 주식가격/주식가격.java
+++ 프로그래머스/lv2/42584. 주식가격/주식가격.java
@@ -3,21 +3,15 @@ import java.util.*;
 class Solution {
     public int[] solution(int[] prices) {
         
-        int[] answer = new int[prices.length];
+        int len = prices.length;
+        int[] answer = new int[len];
         
-        
-        for(int i=0; i<prices.length-1; i++) {
-            int cnt = 0;
-            for(int j=i+1; j<prices.length; j++) {
-                if(prices[i] <= prices[j]) {
-                    cnt++;
-                } else {
-                    cnt++;
+        for (int i = 0; i < len; i++) {
+            for (int j = i + 1; j < len; j++) {
+                answer[i]++;
+                if (prices[i] > prices[j])
                     break;
-                }
             }
-            answer[i] = cnt;
-            // System.out.println(answer[i]);
         }
         return answer;
     }
