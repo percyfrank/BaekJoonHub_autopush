ddd9ff3de6530f46daa8aa4ba8537a00fd14badb
ohsuk Kwon <85394884+percyfrank@users.noreply.github.com>
2023-02-28 10:08:17 +0900

    [level 1] Title: 하샤드 수, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12947. 하샤드 수/하샤드 수.py
@@ -0,0 +1,10 @@
+def solution(x):
+    answer = True
+    tmp = x
+    sum = 0
+    while(x > 0):
+        sum += x%10
+        x //= 10
+    if(tmp % sum != 0):
+        return False
+    return answer
