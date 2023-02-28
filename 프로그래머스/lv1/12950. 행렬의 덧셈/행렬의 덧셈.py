c589e4f9c75ac2115ad2884b85b7e6aba6c18e3c
ohsuk Kwon <85394884+percyfrank@users.noreply.github.com>
2023-02-28 10:51:09 +0900

    [level 1] Title: 행렬의 덧셈, Time: 33.31 ms, Memory: 22.7 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12950. 행렬의 덧셈/행렬의 덧셈.py
@@ -0,0 +1,8 @@
+def solution(arr1, arr2):
+    row = len(arr1)
+    col = len(arr1[1])
+    answer = [[0] * col for _ in range(row)]
+    for i in range(row):
+        for j in range(col):
+            answer[i][j] = arr1[i][j] + arr2[i][j]
+    return answer
