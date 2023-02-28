3a9da462fa474b7ce22e0f5cd3ce0b552c972148
ohsuk Kwon <85394884+percyfrank@users.noreply.github.com>
2023-02-28 11:48:33 +0900

    [level 1] Title: 시저 암호, Time: 1.95 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv1/12926. 시저 암호/시저 암호.py
+++ 프로그래머스/lv1/12926. 시저 암호/시저 암호.py
@@ -3,10 +3,10 @@ def solution(s, n):
     answer = ''
     
     for a in s:
-        if(ord('a') <= ord(a) <= ord('z')):
-            answer += chr(ord('a') + (ord(a) + n -ord('a')) % 26)
-        elif(ord('A') <= ord(a) <= ord('Z')):
-            answer += chr(ord('A') + (ord(a) + n -ord('A')) % 26)
+        if('a' <= a <= 'z'):
+            answer += chr(ord('a') + (ord(a) + n - ord('a')) % 26)
+        elif('A' <= a <= 'Z'):
+            answer += chr(ord('A') + (ord(a) + n - ord('A')) % 26)
         elif(a == ' '):
             answer += ' '
         
