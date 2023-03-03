db2e69801c5e35ceaada4c22c7dca1914f15cb26
ohsuk Kwon <85394884+percyfrank@users.noreply.github.com>
2023-03-03 11:58:47 +0900

    [level 1] Title: 모의고사, Time: 2.41 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/lv1/42840. 모의고사/모의고사.py
+++ 프로그래머스/lv1/42840. 모의고사/모의고사.py
@@ -1,6 +1,5 @@
 def solution(answers):
 
-    answer = []
     a = [1,2,3,4,5]
     b = [2,1,2,3,2,4,2,5]
     c = [3,3,1,1,2,2,4,4,5,5]
@@ -9,14 +8,12 @@ def solution(answers):
     for i in range(len(answers)):
         if a[i%5] == answers[i]:
             records[0] += 1
-    for i in range(len(answers)):
         if b[i%8] == answers[i]:
             records[1] += 1
-    for i in range(len(answers)):
         if c[i%10] == answers[i]:     
             records[2] += 1
-        
 
+    answer = []
     for i in range(3):
         if records[i] == max(records):
             answer.append(i+1)
