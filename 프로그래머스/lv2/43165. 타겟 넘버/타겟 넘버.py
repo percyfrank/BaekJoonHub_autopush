06532f960d5e4b6df0dfcb352208ea774b37d2b5
ohsuk Kwon <85394884+percyfrank@users.noreply.github.com>
2023-03-13 16:06:12 +0900

    [level 2] Title: 타겟 넘버, Time: 1.20 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/43165. 타겟 넘버/타겟 넘버.py
@@ -0,0 +1,12 @@
+def solution(numbers, target):
+    answer = 0
+    
+    a = [0]
+    for i in numbers:
+        b = []
+        for j in a:
+            b.append(j+i)
+            b.append(j-i)
+        a = b
+    
+    return a.count(target)
