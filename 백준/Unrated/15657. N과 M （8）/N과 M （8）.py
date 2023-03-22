f7b3c4d62b955353b5b0bf4cdd53ad66960bd71c
ohsuk Kwon <85394884+percyfrank@users.noreply.github.com>
2023-03-22 12:01:27 +0900

    [Unrated] Title: N과 M (8), Time: 60 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Unrated/15657. N과 M （8）/N과 M （8）.py
@@ -0,0 +1,14 @@
+def solve(start):
+    if len(answer) == m:
+        print(*answer)
+        return
+    for i in range(start, len(arr)):
+        answer.append(arr[i])
+        solve(i)
+        answer.pop()
+
+n,m = map(int,input().split())
+arr = list(map(int,input().split()))
+arr.sort()
+answer = []
+solve(0)
