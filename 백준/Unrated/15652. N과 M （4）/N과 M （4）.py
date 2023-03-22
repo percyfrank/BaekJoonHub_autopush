552d28d1b6e0278cab38735b61bd13e7e4fc5bb1
ohsuk Kwon <85394884+percyfrank@users.noreply.github.com>
2023-03-22 10:30:24 +0900

    [Unrated] Title: N과 M (4), Time: 60 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Unrated/15652. N과 M （4）/N과 M （4）.py
@@ -0,0 +1,12 @@
+def solve(start):
+    if len(answer) == m:
+        print(*answer)
+        return
+    for num in range(start,n+1):
+            answer.append(num)
+            solve(num)
+            answer.pop()
+
+n,m = map(int,input().split())
+answer = []
+solve(1)
