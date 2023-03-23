e5f9a145cf0e6fe9f690dfb5df18f20bfdc5e53b
ohsuk Kwon <85394884+percyfrank@users.noreply.github.com>
2023-03-23 19:47:22 +0900

    [Gold III] Title: 소문난 칠공주, Time: 3508 ms, Memory: 153760 KB -BaekjoonHub

---
+++ 백준/Gold/1941. 소문난 칠공주/소문난 칠공주.py
@@ -0,0 +1,52 @@
+from collections import deque
+
+graph = [list(map(str,input())) for _ in range(5)]
+answer = 0
+arr = []
+
+def check(arr):
+    dx = [-1,1,0,0]
+    dy = [0,0,-1,1]
+    visited = [False] * 7
+    q = deque()
+    q.append(arr[0])
+    visited[0] = True
+
+    while q:
+        x,y = q.popleft()
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+            if (nx,ny) in arr and 0 <= nx < 5 and 0 <= ny < 5:
+                idx = arr.index((nx,ny))
+                if not visited[idx]:
+                    visited[idx] = True
+                    q.append((nx,ny))
+
+    if False in visited:
+        return False
+    else:
+        return True
+
+possible = [[False] * 5 for _ in range(5)]
+def solve(depth, startIdx, cntY):
+    global answer
+
+    if cntY >= 4:
+        return
+
+    if depth == 7:
+        if check(arr):
+            answer += 1
+
+    for i in range(startIdx,25):
+        x = i // 5
+        y = i % 5
+        arr.append((x,y))
+        possible[x][y] = True
+        solve(depth+1, i+1, cntY + (graph[x][y] == 'Y'))
+        possible[x][y] = False
+        arr.pop()
+
+solve(0,0,0)
+print(answer)
