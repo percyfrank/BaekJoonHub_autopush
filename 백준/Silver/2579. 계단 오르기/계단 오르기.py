4f2ded0dea81a494a29bdc26d9d767833c55d07e
ohsuk Kwon <85394884+percyfrank@users.noreply.github.com>
2023-04-01 17:53:31 +0900

    [Silver III] Title: 계단 오르기, Time: 52 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/2579. 계단 오르기/계단 오르기.py
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.py
@@ -1,17 +1,18 @@
 n = int(input())
-s = [0 for _ in range(n)]
-for i in range(n):
+s = [0 for _ in range(n+1)]
+for i in range(1,n+1):
     s[i] = int(input())
-
-dp = [0 for _ in range(n)]
+dp = [0 for _ in range(n+1)]
 
 if n <= 2:
     print(sum(s))
     exit()
-else:
-    dp[0] = s[0]
-    dp[1] = s[0] + s[1]
-    dp[2] = max(s[0]+s[2],s[1]+s[2])
-    for i in range(3,n):
-        dp[i] = max(dp[i-3]+s[i-1]+s[i],dp[i-2]+s[i])
-    print(dp[n-1])
+
+dp[1] = s[1]
+dp[2] = s[2]
+dp[3] = s[3]
+
+for i in range(4,n+1):
+    dp[i] = min(dp[i-2],dp[i-3]) + s[i]
+
+print(sum(s) - min(dp[n-2],dp[n-1]))
