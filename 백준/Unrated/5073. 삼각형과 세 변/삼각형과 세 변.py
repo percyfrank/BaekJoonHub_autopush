00121b3e137224ebedda98f0be3985660b1a1187
ohsuk Kwon <85394884+percyfrank@users.noreply.github.com>
2023-04-03 10:43:59 +0900

    [Unrated] Title: 삼각형과 세 변, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Unrated/5073. 삼각형과 세 변/삼각형과 세 변.py
@@ -0,0 +1,30 @@
+def isTrianlge(arr):
+    if arr[0] + arr[1] <= arr[2]:
+        return True
+
+def solve(arr):
+    cnt = 0
+    for i in range(2):
+        if arr[i] == arr[i+1]:
+            cnt += 1
+    if cnt == 0:
+        print('Scalene')
+    elif cnt == 1:
+        print("Isosceles")
+    else:
+        print("Equilateral")
+
+
+while(True):
+    arr = list(map(int,input().split()))
+    arr.sort()
+    cnt = 0
+    for data in arr:
+        if data == 0:
+            cnt += 1
+    if cnt == 3:
+        break
+    if isTrianlge(arr):
+        print("Invalid")
+        continue
+    solve(arr)
