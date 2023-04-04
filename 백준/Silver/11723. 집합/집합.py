71fc894782956d0304340b85ac519e3eaefbeba8
ohsuk Kwon <85394884+percyfrank@users.noreply.github.com>
2023-04-04 10:43:32 +0900

    [Silver V] Title: 집합, Time: 3496 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/11723. 집합/집합.py
+++ 백준/Silver/11723. 집합/집합.py
@@ -7,23 +7,20 @@ arr = []
 
 for i in range(m):
     arr = input().split()
-    if len(arr) == 2:
-        arr[1] = int(arr[1])
     if arr[0] == 'check':
-        if arr[1] in s:
+        if int(arr[1]) in s:
             print(1)
         else:
             print(0)
     elif arr[0] == 'add':
-        s.add(arr[1])
-    elif arr[0] == 'remove':
-        if arr[1] in s:
-            s.remove(arr[1])
+        s.add(int(arr[1]))
+    elif arr[0] == 'remove' and int(arr[1]) in s:
+        s.remove(int(arr[1]))
     elif arr[0] == 'toggle':
-        if arr[1] in s:
-            s.remove(arr[1])
+        if int(arr[1]) in s:
+            s.remove(int(arr[1]))
         else:
-            s.add(arr[1])
+            s.add(int(arr[1]))
     elif arr[0] == 'all':
         s = set(range(1,21))
     elif arr[0] == 'empty':
