f3d2ba829fa47bf5ea875287d81cb3cac8e29795
ohsuk Kwon <85394884+percyfrank@users.noreply.github.com>
2023-04-04 18:39:24 +0900

    [Silver V] Title: 임스와 함께하는 미니게임, Time: 80 ms, Memory: 42524 KB -BaekjoonHub

---
+++ 백준/Silver/25757. 임스와 함께하는 미니게임/임스와 함께하는 미니게임.py
@@ -0,0 +1,16 @@
+import sys
+input = sys.stdin.readline
+
+n,game = input().split()
+
+player = set()
+for _ in range(int(n)):
+    player.add(input())
+
+cnt = len(player)
+if game == 'Y':
+    print(cnt // 1)
+elif game == 'F':
+    print(cnt // 2)
+elif game == 'O':
+    print(cnt // 3)
