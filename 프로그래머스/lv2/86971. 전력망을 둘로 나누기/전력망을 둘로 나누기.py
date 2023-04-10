d5224ac9dc0ef04b13a4723ff07346be95ef5da3
ohsuk Kwon <85394884+percyfrank@users.noreply.github.com>
2023-04-10 16:53:32 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 3.39 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.py
+++ 프로그래머스/lv2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.py
@@ -7,19 +7,11 @@ def solution(n, wires):
         arr[a].append(b)
         arr[b].append(a)
     
-    def bfs(start):
-        visited = [0 for _ in range(n+1)]
-        q = deque()
-        q.append(start)
+    def dfs(start,cnt):
         visited[start] = 1
-        cnt = 0
-        while q:
-            node = q.popleft()
-            for i in arr[node]:
-                if not visited[i]:
-                    visited[i] = 1
-                    q.append(i)
-                    cnt += 1  
+        for i in arr[start]:
+            if not visited[i]:
+                cnt = dfs(i,cnt+1)
         return cnt
     
     answer = n
@@ -27,7 +19,9 @@ def solution(n, wires):
         arr[a].remove(b)
         arr[b].remove(a)
         
-        answer = min(abs(bfs(a)-bfs(b)),answer)
+        cnt = 0
+        visited = [0 for _ in range(n+1)]
+        answer = min(abs(dfs(a,cnt)-dfs(b,cnt)), answer)
         
         arr[a].append(b)
         arr[b].append(a)
