86feb7fe287902fd54c1ef5b70d740a5db909d68
ohsuk Kwon <85394884+percyfrank@users.noreply.github.com>
2023-04-11 19:22:57 +0900

    [level 2] Title: 이진 변환 반복하기, Time: 0.19 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv2/70129. 이진 변환 반복하기/이진 변환 반복하기.py
+++ 프로그래머스/lv2/70129. 이진 변환 반복하기/이진 변환 반복하기.py
@@ -3,14 +3,10 @@ def solution(s):
     
     cal = 0
     cnt = 0
-    while True:
-        tmp = 0
-        for data in s:
-            if data == "0":
-                tmp += 1
+    while s != "1":
+        tmp = s.count("0")
         cnt += tmp
         len_s = len(s) - tmp
-        s  = "1" * len_s
 
         arr = []
         while len_s > 0:
@@ -20,8 +16,6 @@ def solution(s):
         s = ''.join(reversed(arr))
         
         cal += 1
-        if s == "1":
-            break
         
     answer.append(cal)
     answer.append(cnt)
