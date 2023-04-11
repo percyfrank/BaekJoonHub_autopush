13ff71ae734b876f07171730c63ddc60dc73a471
ohsuk Kwon <85394884+percyfrank@users.noreply.github.com>
2023-04-11 22:26:28 +0900

    [level 2] Title: 다음 큰 숫자, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv2/12911. 다음 큰 숫자/다음 큰 숫자.py
+++ 프로그래머스/lv2/12911. 다음 큰 숫자/다음 큰 숫자.py
@@ -1,25 +1,7 @@
 def solution(n):
-    answer = 0
-    start = n+1
-    
-    tmp = []
-    while n > 0:
-        tmp.append(n % 2)
-        n //= 2
-    cnt = tmp.count(1)
-    
+    num1 = bin(n).count('1')
     while True:
-        
-        ans = start
-        arr = []
-        while ans > 0:
-            arr.append(ans % 2)
-            ans //= 2
-        
-        if arr.count(1) == cnt:
-            answer = start
+        n = n + 1
+        if num1 == bin(n).count('1'):
             break
-            
-        start += 1
-    
-    return answer
+    return n
