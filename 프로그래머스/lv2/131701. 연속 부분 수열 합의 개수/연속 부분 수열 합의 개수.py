396a936cb2f244738b3ea0d3b6ff965d2f784572
ohsuk Kwon <85394884+percyfrank@users.noreply.github.com>
2023-04-17 12:10:27 +0900

    [level 2] Title: 연속 부분 수열 합의 개수, Time: 5307.00 ms, Memory: 43.6 MB -BaekjoonHub

--- 프로그래머스/lv2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.py
+++ 프로그래머스/lv2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.py
@@ -2,12 +2,10 @@ def solution(elements):
     
     arr = []
     num = set()
+    tmp = elements * 2
+    
     for i in range(len(elements)):
-        tmp = elements.copy()
-        for j in range(i):
-            tmp.append(elements[j])
-        for k in range(len(tmp)-i):
-            num.add(sum(tmp[k:k+i]))
-        
+        for j in range(len(tmp)-i):
+            num.add(sum(tmp[j:j+i]))
         
     return len(num)
