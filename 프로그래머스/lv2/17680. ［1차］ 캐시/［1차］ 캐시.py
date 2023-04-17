74f4fee592939490054ca331a41b7da4f3b1194b
ohsuk Kwon <85394884+percyfrank@users.noreply.github.com>
2023-04-17 11:26:26 +0900

    [level 2] Title: [1차] 캐시, Time: 0.68 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv2/17680. ［1차］ 캐시/［1차］ 캐시.py
+++ 프로그래머스/lv2/17680. ［1차］ 캐시/［1차］ 캐시.py
@@ -6,22 +6,18 @@ def solution(cacheSize, cities):
         cities[i] = cities[i].lower()
         
     answer = 0
-    cache = deque()
+    cache = deque(maxlen=cacheSize)
+    
     if cacheSize == 0:
         answer += len(cities) * 5
     else:
         for data in cities:
             if data not in cache: 
-                if len(cache) + 1 <= cacheSize:
-                    cache.append(data)
-                    answer += 5
-                else:
-                    cache.popleft()
-                    cache.append(data)
-                    answer += 5
+                cache.append(data)
+                answer += 5
             else:
                 cache.remove(data)
                 cache.append(data)
                 answer += 1
-
+                
     return answer
