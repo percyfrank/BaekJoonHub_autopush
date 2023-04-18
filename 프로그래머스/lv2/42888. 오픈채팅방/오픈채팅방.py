101cd26befa8c777158ade73eb634e9d54bef38b
ohsuk Kwon <85394884+percyfrank@users.noreply.github.com>
2023-04-18 17:48:44 +0900

    [level 2] Title: 오픈채팅방, Time: 143.36 ms, Memory: 38.2 MB -BaekjoonHub

--- 프로그래머스/lv2/42888. 오픈채팅방/오픈채팅방.py
+++ 프로그래머스/lv2/42888. 오픈채팅방/오픈채팅방.py
@@ -1,24 +1,17 @@
 def solution(record):
-    userDict = {}
     answer = []
 
-    for line in record:
-        line = line.split(" ")
-
-        if line[0] == "Enter":
-            userDict[line[1]] = line[2]
-        elif line[0] == "Change":
-            userDict[line[1]] = line[2]
-
-    for line in record:
-        line = line.split(" ")
-        targetString = userDict[line[1]]
-        if line[0] == "Enter":
-            targetString += "님이 들어왔습니다."
-        elif line[0] == "Leave":
-            targetString += "님이 나갔습니다."
-        else:
-            continue
-        answer.append(targetString)
-
+    arr = []
+    name = {}  
+    for data in record:
+        if len(data.split()) == 3:
+            name[data.split()[1]] = data.split()[2]
+            
+    for data in record:
+        user = data.split()[1]
+        if data.split()[0] == "Enter":
+            answer.append(name[user] + "님이 들어왔습니다.")
+        elif data.split()[0] == "Leave":
+            answer.append(name[user] + "님이 나갔습니다.")
+    
     return answer
