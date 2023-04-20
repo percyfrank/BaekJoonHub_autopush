baa2c7d76e6cd4e3e5370e90c4d50b731b7a0a9e
ohsuk Kwon <85394884+percyfrank@users.noreply.github.com>
2023-04-20 19:06:41 +0900

    [level 2] Title: [3차] 파일명 정렬, Time: 2.43 ms, Memory: 10.7 MB -BaekjoonHub

--- 프로그래머스/lv2/17686. ［3차］ 파일명 정렬/［3차］ 파일명 정렬.py
+++ 프로그래머스/lv2/17686. ［3차］ 파일명 정렬/［3차］ 파일명 정렬.py
@@ -1,23 +1,9 @@
+import re
 def solution(files):
-
-    arr = []
-    for data in files:
-        head,number,tail = '','',''
-        for i in range(len(data)):
-            if data[i].isdigit():
-                number = data[i:]
-                head = data[:i]
-                break
-        for j in range(len(number)):
-            if not number[j].isdigit():
-                tail = number[j:]
-                number = number[:j]
-                break
-        arr.append([head,number,tail])
-    arr.sort(key=lambda x:(x[0].lower(),int(x[1])))
     
-    answer = []
-    for data in arr:
-        answer.append(''.join(data))
-        
-    return answer
+    tmp = [re.split('(\d+)', file) for file in files]
+    print(tmp)
+    
+    sort = sorted(tmp, key = lambda x: (x[0].lower(), int(x[1])))
+    
+    return [''.join(s) for s in sort]
