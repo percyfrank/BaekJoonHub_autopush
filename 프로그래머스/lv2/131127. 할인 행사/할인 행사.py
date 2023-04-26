43de11aca7d632a277460ca4c78d05b501344433
ohsuk Kwon <85394884+percyfrank@users.noreply.github.com>
2023-04-26 16:43:00 +0900

    [level 2] Title: 할인 행사, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv2/131127. 할인 행사/할인 행사.py
+++ 프로그래머스/lv2/131127. 할인 행사/할인 행사.py
@@ -1,21 +1,22 @@
 def solution(want, number, discount):
     answer = 0
     
-    total = dict()
+    cart = dict()
+    for i in range(len(want)):
+        cart[want[i]] = number[i]
+    
     for i in range(len(discount)-9):
-        total = {a:b for a,b in zip(want,number)}
+        tmp = cart.copy()
         for j in range(i,i+10):
-            if discount[j] in total.keys():
-                total[discount[j]] -= 1
-        
+            if discount[j] in tmp.keys():
+                tmp[discount[j]] -= 1
         cnt = 0
-        for value in total.values():
+        for _,value in tmp.items():
             if value == 0:
                 cnt += 1
         if cnt == len(want):
             answer += 1
             
+    
+    
     return answer
-
-
-
