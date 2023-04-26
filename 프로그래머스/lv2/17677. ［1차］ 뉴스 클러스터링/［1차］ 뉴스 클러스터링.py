17c51a48bd3bfbdba005228731fc15efcac7b4b8
ohsuk Kwon <85394884+percyfrank@users.noreply.github.com>
2023-04-26 16:23:14 +0900

    [level 2] Title: [1차] 뉴스 클러스터링, Time: 0.10 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv2/17677. ［1차］ 뉴스 클러스터링/［1차］ 뉴스 클러스터링.py
+++ 프로그래머스/lv2/17677. ［1차］ 뉴스 클러스터링/［1차］ 뉴스 클러스터링.py
@@ -1,19 +1,18 @@
 def solution(str1, str2):
     
-    
-    list1 = [str1[i:i+2].lower() for i in range(len(str1)-1) if str1[i:i+2].isalpha()]
-    list2 = [str2[i:i+2].lower() for i in range(len(str2)-1) if str2[i:i+2].isalpha()]
+    arr1 = [str1[i:i+2].lower() for i in range(len(str1)-1) if str1[i:i+2].isalpha()] 
+    arr2 = [str2[i:i+2].lower() for i in range(len(str2)-1) if str2[i:i+2].isalpha()]
 
-    total = set(list1) | set(list2)
-    A = 0
-    B = 0
-    
-    if total:
-        for words in total:
-            A += min(list1.count(words),list2.count(words))
-            B += max(list1.count(words),list2.count(words))
-        return int(A/B * 65536)
-    else:
+    s1 = set(arr1) & set(arr2)
+    s2 = set(arr1) | set(arr2)
+
+    a,b = 0,0
+    for data in s1:
+        a += min(arr1.count(data),arr2.count(data))
+    for data in s2:
+        b += max(arr1.count(data),arr2.count(data))
+        
+    if a == b == 0:
         return 65536
     
-    
+    return int((a/b) * 65536)
