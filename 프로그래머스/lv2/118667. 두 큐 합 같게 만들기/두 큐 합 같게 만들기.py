ae61d621bb4a50d1813f2bb526c36c71251908cd
ohsuk Kwon <85394884+percyfrank@users.noreply.github.com>
2023-05-07 22:46:56 +0900

    [level 2] Title: 두 큐 합 같게 만들기, Time: 53.88 ms, Memory: 20.9 MB -BaekjoonHub

--- 프로그래머스/lv2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.py
+++ 프로그래머스/lv2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.py
@@ -3,36 +3,47 @@ from collections import deque
 def solution(queue1, queue2):
 
     answer = 0
-    sum1 = sum(queue1)
-    sum2 = sum(queue2)
+    sum1, sum2 = sum(queue1), sum(queue2)
+    target = sum1 + sum2
+    queue1, queue2 = deque(queue1), deque(queue2) 
     
-    queue1 = deque(queue1)
-    queue2 = deque(queue2)
-    
-    if (sum1+sum2) % 2 == 1:
+    if target % 2 != 0:
         return -1
     else:
-        if max(queue1+queue2) > (sum1+sum2) // 2:
+        if max(queue1+queue2) > target // 2:
             return -1
     
-    while True:
-        
-        if sum1 > sum2:
+    while answer < 600000:
+        if sum1 == sum2:
+            return answer
+        elif sum1 > sum2:
             tmp = queue1.popleft()
             sum2 += tmp
             sum1 -= tmp
             queue2.append(tmp)
-            answer += 1
         elif sum1 < sum2:
             tmp = queue2.popleft()
             sum1 += tmp
             sum2 -= tmp
             queue1.append(tmp)
-            answer += 1
-        else:
-            break
-        
-        if answer == 4 * len(queue1):
-            return -1
-        
-    return answer
+        answer += 1
+    
+    return -1
+    
+#     while True:
+#         if sum1 
+#         if sum1 > sum2:
+            # tmp = queue1.popleft()
+            # sum2 += tmp
+            # sum1 -= tmp
+            # queue2.append(tmp)
+            # answer += 1
+#         elif sum1 < sum2:
+            # tmp = queue2.popleft()
+            # sum1 += tmp
+            # sum2 -= tmp
+            # queue1.append(tmp)
+            # answer += 1
+#         else:
+#             return answer
+    
