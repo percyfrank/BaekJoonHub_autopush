82b75c5f7a1228beeed9d613ff41aa2cafd03c00
ohsuk Kwon <85394884+percyfrank@users.noreply.github.com>
2023-05-12 14:23:53 +0900

    [Silver II] Title: 로또, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/6603. 로또/로또.py
@@ -0,0 +1,18 @@
+def solve(start):
+    if len(answer) == 6:
+        print(*answer)
+        return
+
+    for i in range(start,len(arr)):
+        answer.append(arr[i])
+        solve(i+1)
+        answer.pop()
+
+while True:
+    arr = list(map(int,input().split()))
+    if arr[0] == 0:
+        break
+    arr = arr[1:]
+    answer = []
+    solve(0)
+    print()
