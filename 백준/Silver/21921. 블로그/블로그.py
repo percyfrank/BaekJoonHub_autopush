d8fce57653c498b575d81d22a36eb26c8ff382d5
ohsuk Kwon <85394884+percyfrank@users.noreply.github.com>
2023-05-12 18:06:20 +0900

    [Silver III] Title: 블로그, Time: 184 ms, Memory: 60696 KB -BaekjoonHub

--- 백준/Silver/21921. 블로그/블로그.py
+++ 백준/Silver/21921. 블로그/블로그.py
@@ -12,13 +12,12 @@ for i in range(x,n+1):
     arr.append(prefix[i] - prefix[i-x])
 
 MAX = max(arr)
-cnt = 0
-for data in arr:
-    if data == MAX:
-        cnt += 1
-
 if MAX == 0:
     print("SAD")
 else:
+    cnt = 0
+    for data in arr:
+        if data == MAX:
+            cnt += 1
     print(MAX)
     print(cnt)
