6ffe0c939c2989e4082a4374bcd6002c6942e0cf
ohsuk Kwon <85394884+percyfrank@users.noreply.github.com>
2023-05-12 16:35:04 +0900

    [Silver III] Title: 영단어 암기는 괴로워, Time: 640 ms, Memory: 153416 KB -BaekjoonHub

---
+++ 백준/Silver/20920. 영단어 암기는 괴로워/영단어 암기는 괴로워.py
@@ -0,0 +1,13 @@
+n,m = map(int,input().split())
+words = {}
+for _ in range(n):
+    tmp = input()
+    if len(tmp) < m:
+        continue
+    words.setdefault(tmp,0)
+    words[tmp] += 1
+
+answer = list(sorted(words.items(),key=lambda x:(-x[1],-len(x[0]),x[0])))
+
+for data in answer:
+    print(data[0])
